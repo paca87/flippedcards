cd394f7947a706595bd2aa0d87705512f598dd50
paca <frpacareu87@gmail.com>
2020-04-17 18:45:29 -0700

    input and flexbox

--- main.js
+++ main.js
@@ -2,133 +2,135 @@
 camel case listaFotos
 snake case lista_fotos
 Camel cas is the most use
-*/
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-}
-/**
+------
+/*
  * paso 1: Crear arreglo dinamico de Pictures
  * paso 2: Insertar en el DOM images div->img https://www.w3schools.com/jsref/met_document_createelement.asp
- * paso 3: Caputar el evento
+ * paso 3: Caputar el evento y hacer q las fotos esten visibles o no utilizando css(propiedad visibility: hidden)
  */
-const listPictures = [
-    {
-        name: 'pic1',
-        flipped: false,
-        hidden: false,
-    },
-    {
-        name: 'pic2',
-        flipped: false,
-    },
-    {
-        name: 'pic3',
-        flipped: false,
-    },
-    {
-        name: 'pic1',
-        flipped: false,
-    },
-    {
-        name: 'pic2',
-        flipped: false,
-    },
-    {
-        name: 'pic3',
-        flipped: false,
-    },
 
-]
-const dirF1 = '/javi/img/pic1.jpg';
-const dirF2 = '/javi/img/pic2.jpg';
-const dirF3 = '/javi/img/pic3.jpg';
-const dirBack = '/javi/img/back.jpg';
-let fronts = [1, 2, 3, 4, 5, 6];
-const tablero = document.querySelector(`.tablero`);
-const listaFotos = document.querySelectorAll(`.foto`);
-colocarFotos();
-//pongo las cartas boca abajo
-listaFotos.forEach(function (foto) {
-    foto.innerHTML = `<img src="${dirBack}"alt="">`;
-});
-let javier = 5;
+//*************************************Principal
+
+//const dirBack = '/javi/img/back.jpg';
+let score = false;
+let listPictures = [];
+let isSecondCard = false;
+//console.log(document.querySelector('#cantFotos')[0].value);
+crearListaFotos();
+const tablero = document.querySelector(`.table`);
+document.querySelector("#btn").addEventListener("click", newGameHandler);
+setTable();
+console.log(tablero);
+console.log(listPictures);
+
+//*************************************Auxiliar
+
+/*Luego tengo q Validar q totalFotos sea Par*/
+function crearListaFotos(totalFotos = 6) {
+  listPictures = [];
+
+  let cont = 0;
+  for (let index = 0; index < totalFotos; index++) {
+    const name = `pic${cont}`;
+    const foto = {
+      id: `id${index}`,
+      name: name,
+      flipped: false,
+      hidden: false,
+      locationFront: `https://i.picsum.photos/id/${cont}/100/100.jpg`,
+      locationBack: `/javi/img/back.jpg`,
+    };
+    index % 2 ? cont++ : cont;
+    listPictures.push(foto);
+  }
+  listPictures = shuffle(listPictures);
+}
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
 /*
 coloca las fotos random en la mesa.
 */
-function colocarFotos() {
-    let javier = 6;
-    let veces_pic1 = 0;
-    let veces_pic2 = 0;
-    let veces_pic3 = 0;
-    let cual_li = 0;
-    console.log(javier);
-    while (cual_li < 6) {
-        //console.log(veces_pic1+' '+veces_pic2+' '+veces_pic3+'aaaa');        
-        let ranPoss = fotoValida(veces_pic1, veces_pic2, veces_pic3);
-        //let ranFoto= Math.floor(Math.random() * 3)+1;
-        if (ranPoss == 1 && veces_pic1 < 2) {
-            veces_pic1++;
-            fronts[cual_li] = (`<img src="${dirF1}" id="${cual_li}">`);
-        }
-        else if (ranPoss == 2 && veces_pic2 < 2) {
-            veces_pic2++;
-            fronts[cual_li] = (`<img src="${dirF2}"id="${cual_li}">`);
-        }
-        else if (ranPoss == 3 && veces_pic3 < 2) {
-            veces_pic3++;
-            fronts[cual_li] = (`<img src="${dirF3}"id="${cual_li}">`);
-        }
-        cual_li++;
+function setTable() {
+  tablero.innerHTML = "";
+  listPictures.forEach(function (pic) {
+    let img = document.createElement("img");
+    img.id = pic.id;
+    img.className = pic.name;
+    if (pic.hidden) {
+      img.style.visibility = "hidden";
     }
-    console.log(fronts);
+    img.src = pic.flipped ? pic.locationFront : pic.locationBack;
+    let divFlexBox = document.createElement("div");
+    divFlexBox.className = "container-foto";
+    divFlexBox.appendChild(img);
+    tablero.appendChild(divFlexBox);
+  });
 }
-/*Le paso la cantidad de fotos 1, 2 y3 tengo para 
-q me genere un numero random q me sirva para completar
+/*
+timer
 */
-function fotoValida(pic1, pic2, pic3) {
-    let ranfoto = Math.floor(Math.random() * 3) + 1;
-    //console.log(pic1+' '+pic2+' '+pic3+' foto'+ranfoto);
-    if (ranfoto == 1 && pic1 < 2)
-        return ranfoto;
-    else if (ranfoto == 2 && pic2 < 2)
-        return ranfoto;
-    else if (ranfoto == 3 && pic3 < 2)
-        return ranfoto;
-    else
-        return fotoValida(pic1, pic2, pic3);
+function endGame() {
+  //setinterval
 }
-//const front1=;
+/**
+ * Buscar Pareja
+ */
+function searchPartner(pic1) {
+  for (let i = 0; i < listPictures.length; i++) {
+    let pic2 = listPictures[i];
+    if (pic2.flipped) {
+      if (pic1.id != pic2.id) {
+        if (pic1.name == pic2.name) {
+          pic1.hidden = true;
+          pic2.hidden = true;
+        }
+        pic1.flipped = false;
+        pic2.flipped = false;
+        break;
+      }
+    }
+  }
+}
+/**
+ * Evento click
+ */
+function flipCard(id) {
+  let pic = null;
+  for (let i = 0; i < listPictures.length; i++) {
+    pic = listPictures[i];
+    if (pic.id == id && !pic.flipped) {
+      pic.flipped = true;
+      break;
+    }
+  }
+  return pic;
+}
 
-let click = tablero.addEventListener('click', (evento) => {
-    
-    let que_id = evento.target;
-
-    console.log(que_id);
-    /*console.log(fronts); 
-    if (que_id == 'foto1')
-        que_id = 0
-    else if (que_id == 'foto2')
-        que_id = 1
-    else if (que_id == 'foto3')
-        que_id = 2
-    else if (que_id == 'foto4')
-        que_id = 3
-    else if (que_id == 'foto5')
-        que_id = 4
-    else if (que_id == 'foto6')
-        que_id = 5
-    evento.path[1].innerHTML = fronts[que_id];*/
-
-})
-//console.log(Math.floor(Math.random() * 11); );
-/*foto1[0].innerHTML=`<img src="${dirBack}" alt="">`;
-foto4.innerHTML='<img src="/javi/img/pic3.jpg" alt="">';
-foto6.innerHTML=`<img src="${dirBack}" alt="">`;*/
-
-
-
-//console.log(tablero.firstElementChild)
+let click = tablero.addEventListener("click", (evento) => {
+  let target = evento.target;
+  if (target.tagName == `IMG`) {
+    let pic = flipCard(target.id);
+    setTable();
+    if (isSecondCard) {
+      isSecondCard = false;
+      tablero.style.pointerEvents = "none";
+      setTimeout(() => {
+        searchPartner(pic);
+        setTable();
+        tablero.style.pointerEvents = "auto";
+      }, 2000);
+    } else {
+      isSecondCard = true;
+    }
+  }
+});
+function newGameHandler() {
+  let cantFotos = document.querySelector("#cantFotos").value;
+  crearListaFotos(cantFotos);
+  setTable();
+}
